96215102c8726c62219740c8d3a4c99c60c35845
Julian <josejulianalvarezu@gmail.com>
2023-05-09 17:08:31 -0500

    Pausa

--- js/Arrays.js
+++ js/Arrays.js
@@ -1,4 +1,12 @@
 import {MisArrays,ArraysReferencias} from "./arrays/QueEsUnArray.js";
 
-console.log(MisArrays);
-ArraysReferencias();
+// console.log(MisArrays);
+// ArraysReferencias();
+
+import {EncntrarElSegundoMasGrande, LosCerosoAlFinal, encontrarTresMasGrandes} from "./arrays/ArraysEjercicios.js";
+
+
+
+encontrarTresMasGrandes();
+EncntrarElSegundoMasGrande();
+LosCerosoAlFinal();
