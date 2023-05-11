a94fd0fd7641991169fd84d81e81b261bb40a1d2
Julian <josejulianalvarezu@gmail.com>
2023-05-11 15:45:43 -0500

    Poco a Poco

---
+++ js/TiposDeDatos.js
@@ -0,0 +1,42 @@
+// console.log("variables con VAR")
+
+// var nombre = "Juan";
+// var edad = 30;
+// var ciudad = "Madrid";
+
+// console.log("Mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
+
+
+
+// console.log("*********************************")
+// console.log("Variables con LET")
+
+
+// let nombre2 = "María";
+// let edad2 = 25;
+// let ciudad2 = "Barcelona";
+
+// console.log(`Mi nombre es ${nombre2}, tengo ${edad2} años y vivo en ${ciudad2}.`);
+
+
+
+// console.log("*********************************")
+// console.log("Variables con CONST")
+
+
+// const nombre3 = "Pedro";
+// const edad3 = 40;
+// const ciudad3 = "Valencia";
+
+// console.log(`Mi nombre es ${nombre3}, tengo ${edad3} años y vivo en ${ciudad3}.`);
+
+
+// console.log("*********************************")
+// console.log("Variables con STRING")
+
+// let nombre = "Juan";
+// let mensaje = "Hola, ¿cómo estás?";
+// let direccion = 'Calle Mayor, 10';
+
+
+
