05e67f53b4603cd00b569c03e715d6763159fdfe
Julian <josejulianalvarezu@gmail.com>
2023-05-12 18:50:08 -0500

    Poco a Poco

---
+++ js/LogicaDeProgramación.js
@@ -0,0 +1,98 @@
+//Suma: Calcula La Suma De 5 Y 7.
+
+// let a = 5;
+// let b = 7;
+// let resultado = a + b;
+// console.log(resultado)
+
+
+// Resta: Resta 9 A 15.
+
+// let a = 9;
+// let b = 15;
+// let resultado = a - b;
+// console.log(resultado)
+
+// Multiplicación: Multiplica 4 Por 6.
+
+// let a = 4;
+// let b = 5;
+// let resultado = 4 * 5;
+// console.log(resultado)
+
+// División: Divide 20 Entre 5.
+
+// let a = 20;
+// let b = 5;
+// let resultado = a/b;
+// console.log(resultado)
+
+
+// Módulo: Calcula El Residuo De La División De 17 Entre 3.
+
+// let a = 17;
+// let b = 3;
+// let resultado = a % b;
+// console.log(resultado)
+
+// Potenciación: Eleva 3 Al Cuadrado.
+
+// let a = 3
+// let resultado = a * a;
+// console.log(resultado)
+
+// Incremento: Incrementa En 1 El Valor De Una Variable.
+
+// let a = 3;
+// a++;
+// console.log(a)
+
+// Decremento: Decrementa En 2 El Valor De Una Variable.
+
+// let a = 4;
+// a-=2;
+// console.log(a)
+
+
+// Operaciones Combinadas: Realiza La Operación (4 + 2) * 3 - 5.
+
+// let a = 4, b = 2, c = 3, d = 5;
+// let Operacion = (a + b) * c - d;
+// console.log(Operacion)
+
+// Prioridad De Operaciones: Calcula El Resultado De 10 - 4 * 2.
+
+// let a = 10, b = 4, c = 2;
+// console.log(a - b * c)
+
+// Calcula El Área De Un Rectángulo De Base 8 Y Altura 5.
+
+// let a = 8, b = 5;
+// console.log(a*5)
+
+// Calcula El Perímetro De Un Cuadrado Con Lado De Longitud 6.
+
+// let a = 6;
+// console.log(a*4);
+
+// Calcula El Área De Un Círculo De Radio 3.
+
+// let r = 3;
+// let area = Math.PI * Math.pow(r,2);
+// console.log(area)
+
+// Convierte 50 Grados Celsius A Fahrenheit Usando La Fórmula: F = C * 9/5 + 32.
+// Calcula El Promedio De 4 Números: 8, 5, 12 Y 10.
+// Calcula El Valor Absoluto De -9.
+// Comprueba Si El Número 7 Es Par O Impar.
+// Calcula El Máximo Entre 10 Y 17.
+// Calcula El Mínimo Entre 5, 8, 3 Y 9.
+// Calcula El Resultado De La Expresión (2 + 3) * (6 - 4) / 2.
+// Calcula La Hipotenusa De Un Triángulo Rectángulo Con Catetos De Longitud 3 Y 4.
+// Calcula El Valor De X En La Ecuación 2x + 5 = 13.
+// Comprueba Si El Número 25 Es Divisible Entre 5.
+// Calcula El Resultado De La Expresión (7 + 3) / (2 * 2) + 1.
+// Calcula El Área De Un Triángulo Con Base 6 Y Altura 8.
+// Calcula El Perímetro De Un Rectángulo De Base 10 Y Altura 4.
+// Calcula El Resultado De La Expresión 4 * (3 + 5) - 2.
+// Calcula El Resto De La División De 20 Entre 7.
