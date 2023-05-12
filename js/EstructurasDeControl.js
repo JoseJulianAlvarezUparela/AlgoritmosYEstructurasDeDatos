1260fc1e7831500dba53695bec4287a29f020713
Julian <josejulianalvarezu@gmail.com>
2023-05-11 19:51:58 -0500

    Iniciamos las estrcuturas de control

---
+++ js/EstructurasDeControl.js
@@ -0,0 +1,73 @@
+// let x = 10;
+// let y = 3;
+
+// let suma = x + y; // resultado: 13
+// let resta = x - y; // resultado: 7
+// let multiplicacion = x * y; // resultado: 30
+// let division = x / y; // resultado: 3.3333...
+// let modulo = x % y; // resultado: 1
+// let incremento = ++x; // resultado: 11
+// let decremento = --y; // resultado: 2
+
+
+// let x = 10;
+// let y = 3;
+
+// x += y; // equivalente a x = x + y; resultado: 13
+// x -= y; // equivalente a x = x - y; resultado: 10
+// x *= y; // equivalente a x = x * y; resultado: 30
+// x /= y; // equivalente a x = x / y; resultado: 10
+// x %= y; // equivalente a x = x % y; resultado: 1
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x == y); // resultado: false
+// console.log(x != y); // resultado: true
+// console.log(x === '10'); // resultado: false
+// console.log(x !== '10'); // resultado: true
+// console.log(x > y); // resultado: true
+// console.log(x < y); // resultado: false
+// console.log(x >= y); // resultado: true
+// console.log(x <= y); // resultado: false
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x == y); // resultado: false
+// console.log(x != y); // resultado: true
+// console.log(x === '10'); // resultado: false
+// console.log(x !== '10'); // resultado: true
+// console.log(x > y); // resultado: true
+// console.log(x < y); // resultado: false
+// console.log(x >= y); // resultado: true
+// console.log(x <= y); // resultado: false
+
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x > 5 && y < 5); // resultado: true
+// console.log(x > 5 || y < 5); // resultado: true
+// console.log(!(x > 5)); // resultado: false
+
+
+// let edad = 17;
+
+// let esMayorDeEdad = (edad >= 18) ? 'Sí' : 'No';
+// console.log(esMayorDeEdad); // resultado: No
+
+
+// let x = 10;
+// let y = 'Hola';
+
+// console.log(typeof x); // resultado: number
+// console.log(typeof y); // resultado: string
+// console.log(x instanceof Number); // resultado: false
+// console.log(y instanceof String); // resultado: false
+
+
+
