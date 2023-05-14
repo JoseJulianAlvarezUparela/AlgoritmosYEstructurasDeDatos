eabe75899c5046c81e600e45122230452589faf2
Julian <josejulianalvarezu@gmail.com>
2023-05-14 09:35:39 -0500

    Jose, falta el menu de navegacion

--- js/LogicaDeProgramación.js
+++ js/LogicaDeProgramación.js
@@ -96,3 +96,285 @@
 // Calcula El Perímetro De Un Rectángulo De Base 10 Y Altura 4.
 // Calcula El Resultado De La Expresión 4 * (3 + 5) - 2.
 // Calcula El Resto De La División De 20 Entre 7.
+
+
+
+
+// Operadores De String
+// Concatena Dos Cadenas Usando El Operador +.
+// Asigna Una Cadena A Una Variable Llamada Texto.
+// Obtén La Longitud De La Cadena Texto.
+// Convierte La Cadena Texto A Minúsculas.
+// Convierte La Cadena Texto A Mayúsculas.
+// Extrae Una Subcadena De Texto Desde El Índice 2.
+// Extrae Una Subcadena De Texto Desde El Índice 2 Hasta El 5.
+// Reemplaza Una Parte De La Cadena Texto Por Otra Cadena.
+// Encuentra La Posición De Una Subcadena Dentro De Texto.
+// Verifica Si Texto Comienza Con La Subcadena "Hola".
+// Verifica Si Texto Termina Con La Subcadena "Mundo".
+// Divide Texto En Un Arreglo De Subcadenas Usando Un Separador.
+// Elimina Los Espacios En Blanco Al Inicio Y Al Final De Texto.
+// Obtén El Carácter En El Índice 3 De Texto.
+// Convierte Un Número A Cadena Usando El Método ToString().
+// Verifica Si Una Cadena Contiene Otra Subcadena Usando El Método Includes().
+// Repite La Cadena Texto Tres Veces Usando El Método Repeat().
+// Obtén El Índice De La Primera Aparición De Una Subcadena En Texto.
+// Convierte Una Cadena En Un Arreglo De Caracteres Usando El Método Split().
+// Une Los Elementos De Un Arreglo En Una Cadena Usando El Método Join().
+// Obtén Una Subcadena De Texto Desde El Índice -3.
+// Reemplaza Todas Las Apariciones De Una Subcadena En Texto Por Otra Cadena.
+// Verifica Si Texto Es Igual A Otra Cadena Usando El Operador De Igualdad.
+// Verifica Si Texto Es Diferente A Otra Cadena Usando El Operador De Desigualdad.
+// Obtén Una Subcadena De Texto Con Un Rango De Índices Invertido.
+
+// Arreglos
+// Declara Un Arreglo Llamado Numeros Con Los Valores [1, 2, 3, 4, 5].
+// Accede Al Segundo Elemento Del Arreglo Numeros.
+// Modifica El Tercer Elemento Del Arreglo Numeros Y Asígnale El Valor 10.
+// Agrega Un Nuevo Elemento Con El Valor 6 Al Final Del Arreglo Numeros.
+// Elimina El Último Elemento Del Arreglo Numeros.
+// Declara Un Arreglo Vacío Llamado Frutas.
+// Agrega Los Elementos 'Manzana', 'Banana' Y 'Naranja' Al Arreglo Frutas.
+// Obtén La Longitud Del Arreglo Frutas.
+// Concatena Los Arreglos Numeros Y Frutas.
+// Ordena El Arreglo Numeros De Forma Ascendente.
+// Invierte El Orden De Los Elementos Del Arreglo Frutas.
+// Verifica Si El Elemento 'Banana' Existe En El Arreglo Frutas.
+// Encuentra La Posición Del Elemento 'Naranja' En El Arreglo Frutas.
+// Obtén Los Primeros 3 Elementos Del Arreglo Numeros.
+// Obtén Los Últimos 2 Elementos Del Arreglo Frutas.
+// Remueve El Segundo Elemento Del Arreglo Numeros.
+// Reemplaza El Primer Elemento Del Arreglo Frutas Con El Valor 'Pera'.
+// Une Los Elementos Del Arreglo Frutas En Un String Separado Por Comas.
+// Verifica Si Todos Los Elementos Del Arreglo Numeros Son Mayores A 0.
+// Verifica Si Al Menos Un Elemento Del Arreglo Frutas Comienza Con La Letra 'M'.
+// Multiplica Cada Elemento Del Arreglo Numeros Por 2.
+// Obtén El Valor Máximo Del Arreglo Numeros.
+// Obtén El Valor Mínimo Del Arreglo Numeros.
+// Crea Una Copia Del Arreglo Frutas.
+// Vacía El Arreglo Numeros.
+// Verifica Si El Arreglo Frutas Es Vacío.
+
+
+// Template String
+// Concatena Dos Variables Nombre Y Apellido Utilizando Template Strings.
+// Utiliza Un Template String Para Mostrar Un Mensaje Con El Resultado De Una Operación Matemática.
+// Combina Un String Estático Con El Valor De Una Variable Utilizando Template Strings.
+// Crea Una Plantilla De URL Utilizando Variables Y Template Strings.
+// Genera Un Mensaje Personalizado Utilizando Template Strings Y Variables.
+// Concatena Múltiples Variables Utilizando Template Strings.
+// Crea Un Mensaje De Bienvenida Utilizando Template Strings Y El Valor De Una Variable.
+// Combina Texto Y El Resultado De Una Expresión Matemática Utilizando Template Strings.
+// Utiliza Template Strings Para Mostrar El Contenido De Un Objeto Con Sus Propiedades.
+// Crea Un Mensaje Utilizando Template Strings Y Una Variable Booleana.
+// Concatena Un String Con El Valor Numérico De Una Variable Utilizando Template Strings.
+// Crea Una Lista Desordenada De Elementos Utilizando Template Strings Y Un Array De Datos.
+// Genera Una Tabla HTML Utilizando Template Strings Y Un Array De Objetos.
+// Utiliza Template Strings Para Mostrar Un Mensaje De Error Con Detalles Adicionales.
+// Crea Una Lista Ordenada Numerada Utilizando Template Strings Y Un Array De Datos.
+// Concatena Varios Strings Utilizando Template Strings Y Operadores De Concatenación.
+// Combina Texto Estático Y El Resultado De Una Función Utilizando Template Strings.
+// Utiliza Template Strings Para Mostrar Información De Un Usuario Con Sus Propiedades.
+// Crea Una Tabla HTML Con Filas Dinámicas Utilizando Template Strings Y Un Array De Datos.
+// Genera Un Mensaje Utilizando Template Strings Y Operadores Ternarios.
+// Crea Un Enlace HTML Con Texto Y URL Dinámicos Utilizando Template Strings Y Variables.
+// Concatena El Resultado De Una Función Con Un String Utilizando Template Strings.
+// Utiliza Template Strings Para Mostrar Una Lista De Elementos Con Su Posición Y Valor.
+// Crea Un Mensaje Utilizando Template Strings Y Un Array De Palabras.
+// Combina Texto Y El Resultado De Una Expresión Booleana Utilizando Template Strings.
+
+// Obejtos
+// Crear Un Objeto Llamado Persona Con Propiedades Nombre Y Edad.
+// Acceder Al Valor De La Propiedad Nombre Del Objeto Persona.
+// Modificar El Valor De La Propiedad Edad Del Objeto Persona.
+// Agregar Una Nueva Propiedad Llamada Ciudad Al Objeto Persona.
+// Eliminar La Propiedad Edad Del Objeto Persona.
+// Crear Un Objeto Llamado Libro Con Propiedades Titulo, Autor Y Año.
+// Acceder Al Valor De La Propiedad Autor Del Objeto Libro.
+// Modificar El Valor De La Propiedad Año Del Objeto Libro.
+// Agregar Una Nueva Propiedad Llamada Editorial Al Objeto Libro.
+// Eliminar La Propiedad Titulo Del Objeto Libro.
+// Crear Un Objeto Llamado Producto Con Propiedades Nombre, Precio Y Cantidad.
+// Acceder Al Valor De La Propiedad Precio Del Objeto Producto.
+// Modificar El Valor De La Propiedad Cantidad Del Objeto Producto.
+// Agregar Una Nueva Propiedad Llamada Descripcion Al Objeto Producto.
+// Eliminar La Propiedad Nombre Del Objeto Producto.
+// Crear Un Objeto Llamado Coche Con Propiedades Marca, Modelo Y Año.
+// Acceder Al Valor De La Propiedad Marca Del Objeto Coche.
+// Modificar El Valor De La Propiedad Modelo Del Objeto Coche.
+// Agregar Una Nueva Propiedad Llamada Color Al Objeto Coche.
+// Eliminar La Propiedad Año Del Objeto Coche.
+// Crear Un Objeto Llamado Fruta Con Propiedades Nombre Y Precio.
+// Acceder Al Valor De La Propiedad Nombre Del Objeto Fruta.
+// Modificar El Valor De La Propiedad Precio Del Objeto Fruta.
+// Agregar Una Nueva Propiedad Llamada Color Al Objeto Fruta.
+// Eliminar La Propiedad Nombre Del Objeto Fruta.
+// Crear Un Objeto Llamado Usuario Con Propiedades Nombre, Email Y Contraseña.
+// Acceder Al Valor De La Propiedad Email Del Objeto Usuario.
+// Modificar El Valor De La Propiedad Contraseña Del Objeto Usuario.
+// Agregar Una Nueva Propiedad Llamada Telefono Al Objeto Usuario.
+// Eliminar La Propiedad Nombre Del Objeto Usuario.
+
+// Funciones
+// Suma Dos Números: Sum(A, B)
+// Resta Dos Números: Subtract(A, B)
+// Multiplica Dos Números: Multiply(A, B)
+// Divide Dos Números: Divide(A, B)
+// Obtiene El Residuo De La División De Dos Números: Modulo(A, B)
+// Calcula El Valor Absoluto De Un Número: AbsoluteValue(Num)
+// Calcula La Potencia De Un Número: Power(Base, Exponent)
+// Calcula La Raíz Cuadrada De Un Número: SquareRoot(Num)
+// Calcula El Seno De Un Ángulo En Radianes: Sin(Angle)
+// Calcula El Coseno De Un Ángulo En Radianes: Cos(Angle)
+// Calcula La Tangente De Un Ángulo En Radianes: Tan(Angle)
+// Calcula El Valor Máximo Entre Dos Números: Max(A, B)
+// Calcula El Valor Mínimo Entre Dos Números: Min(A, B)
+// Redondea Un Número Hacia Abajo Al Entero Más Cercano: Floor(Num)
+// Redondea Un Número Hacia Arriba Al Entero Más Cercano: Ceil(Num)
+// Redondea Un Número Al Entero Más Cercano: Round(Num)
+// Calcula El Valor Absoluto Más Grande Entre Dos Números: MaxAbsolute(A, B)
+// Calcula El Valor Mínimo Absoluto Entre Dos Números: MinAbsolute(A, B)
+// Genera Un Número Aleatorio Entre 0 Y 1: Random()
+// Genera Un Número Aleatorio Entre Dos Valores Dados: RandomInRange(Min, Max)
+// Convierte Un Ángulo De Grados A Radianes: DegreesToRadians(Degrees)
+// Convierte Un Ángulo De Radianes A Grados: RadiansToDegrees(Radians)
+// Calcula El Logaritmo Natural De Un Número: NaturalLog(Num)
+// Calcula El Logaritmo En Base 10 De Un Número: Log10(Num)
+// Calcula El Logaritmo En Base 2 De Un Número: Log2(Num)
+
+
+// Operadores De Asignacion
+// Asigna El Valor 5 A Una Variable Llamada X.
+// Incrementa En 2 El Valor De X.
+// Multiplica X Por 3.
+// Divide X Por 4.
+// Resta 1 A X.
+// Asigna El Valor De X A Una Variable Llamada Y.
+// Incrementa Y En 10.
+// Multiplica Y Por 2.
+// Divide Y Por 5.
+// Resta 3 A Y.
+// Asigna El Valor 7 A Una Variable Llamada Z.
+// Suma X Y Z, Y Asigna El Resultado A X.
+// Resta Y Y Z, Y Asigna El Resultado A Y.
+// Multiplica X Por Y Y Asigna El Resultado A X.
+// Incrementa Z En 1.
+// Divide X Entre Z Y Asigna El Resultado A X.
+// Resta Y De X Y Asigna El Resultado A Y.
+// Multiplica X Por 5 Y Asigna El Resultado A X.
+// Divide Y Por 2 Y Asigna El Resultado A Y.
+// Incrementa X En 1 Usando El Operador De Incremento.
+// Resta 1 A Y Usando El Operador De Decremento.
+// Asigna El Valor 2 A X Usando El Operador De Asignación.
+// Multiplica Y Por 3 Usando El Operador De Asignación.
+// Divide X Por 2 Usando El Operador De Asignación.
+// Calcula El Módulo De Y Entre 4 Usando El Operador De Asignación.
+
+// Operaciones De Comparacion
+// Compara Si 5 Es Igual A 5.
+// Compara Si 10 Es Mayor Que 7.
+// Compara Si 3 Es Menor Que 5.
+// Compara Si "Hola" Es Igual A "Hola".
+// Compara Si True Es Igual A False.
+// Compara Si 10 Es Mayor O Igual Que 8.
+// Compara Si 4 Es Menor O Igual Que 2.
+// Compara Si "Perro" Es Distinto De "Gato".
+// Compara Si True Es Estrictamente Igual A True.
+// Compara Si 7 Es Estrictamente Menor Que "7".
+// Compara Si False Es Estrictamente Distinto De 0.
+// Compara Si "Hola" Es Estrictamente Igual A "Hola".
+// Compara Si 12 Es Estrictamente Mayor Que 12.
+// Compara Si 6 Es Estrictamente Menor O Igual Que 6.
+// Compara Si "Verde" Es Estrictamente Distinto De "Azul".
+// Compara Si True Es Igual A 1.
+// Compara Si 9 Es Mayor Que "8".
+// Compara Si Null Es Igual A Undefined.
+// Compara Si "Hola" Es Menor O Igual Que "Adios".
+// Compara Si NaN Es Distinto De NaN.
+// Compara Si True Es Estrictamente Igual A 1.
+// Compara Si 15 Es Estrictamente Menor Que 10.
+// Compara Si "Manzana" Es Estrictamente Distinto De "Manzana".
+// Compara Si False Es Estrictamente Mayor O Igual Que 0.
+// Compara Si Undefined Es Estrictamente Igual A Null.
+// Compara Si "Azul" Es Estrictamente Menor O Igual Que "Azul".
+// Operaciones Logicas
+// Verifica Si 5 Es Mayor Que 3.
+// Verifica Si 10 Es Igual A 10.
+// Verifica Si 7 Es Menor O Igual Que 9.
+// Verifica Si 4 Es Distinto De 6.
+// Verifica Si 8 Es Mayor Que 5 Y Menor Que 10.
+// Verifica Si 6 Es Par.
+// Verifica Si "Hola" Es Igual A "Hola" Y Distinto De "Adiós".
+// Verifica Si 20 Es Divisible Por 5 O 3.
+// Verifica Si Al Menos Uno De Los Siguientes Es Verdadero: True, False, True.
+// Verifica Si Todos Los Siguientes Son Verdaderos: True, True, True.
+// Verifica Si Al Menos Uno De Los Siguientes Es Falso: False, True, True.
+// Verifica Si Todos Los Siguientes Son Falsos: False, False, False.
+// Verifica Si Al Menos Uno De Los Siguientes Es Verdadero: 0, False, Null.
+// Verifica Si Todos Los Siguientes Son Verdaderos: "Hola", True, 1.
+// Verifica Si Al Menos Uno De Los Siguientes Es Falso: "", 0, Undefined.
+// Verifica Si Todos Los Siguientes Son Falsos: NaN, False, Null.
+// Verifica Si Al Menos Uno De Los Siguientes Es Verdadero: !True, !!False, !!!True.
+// Verifica Si Todos Los Siguientes Son Verdaderos: !False, !!True, !!!False.
+// Verifica Si Al Menos Uno De Los Siguientes Es Falso: !True, !!False, !!!False.
+// Verifica Si Todos Los Siguientes Son Falsos: !True, !!False, !!!True.
+// Verifica Si Al Menos Uno De Los Siguientes Es Verdadero: Boolean(0), Boolean(""), Boolean(Null).
+// Verifica Si Todos Los Siguientes Son Verdaderos: Boolean(1), Boolean("Hola"), Boolean([]).
+// Verifica Si Al Menos Uno De Los Siguientes Es Falso: Boolean(Undefined), Boolean(NaN), Boolean("").
+// Verifica Si Todos Los Siguientes Son Falsos: Boolean(False), Boolean(Null), Boolean(0).
+// Verifica Si Al Menos Uno De Los Siguientes Es Verdadero: Boolean(Undefined) || Boolean(Null).
+// Verifica Si Todos Los Siguientes Son Verdaderos: Boolean(True) && Boolean(1).
+
+// Operadores Ternarios
+// Comprueba Si Una Variable X Es Mayor Que 10. Si Es Cierto, Asigna 1 A Otra Variable Llamada Resultado; De Lo Contrario, Asigna 0.
+// Comprueba Si Una Variable Edad Es Mayor O Igual A 18. Si Es Cierto, Asigna "Mayor De Edad" A Otra Variable Llamada Mensaje; De Lo Contrario, Asigna "Menor De Edad".
+// Comprueba Si Una Variable EsValido Es Verdadera. Si Es Cierto, Asigna "Válido" A Otra Variable Llamada Estado; De Lo Contrario, Asigna "Inválido".
+// Comprueba Si Una Variable Numero Es Par. Si Es Cierto, Asigna "Par" A Otra Variable Llamada Tipo; De Lo Contrario, Asigna "Impar".
+// Comprueba Si Una Variable Puntuacion Es Mayor O Igual A 60. Si Es Cierto, Asigna "Aprobado" A Otra Variable Llamada Resultado; De Lo Contrario, Asigna "Reprobado".
+// Comprueba Si Una Variable Temperatura Es Mayor A 25. Si Es Cierto, Asigna "Calor" A Otra Variable Llamada Clima; De Lo Contrario, Asigna "Frio".
+// Comprueba Si Una Variable EsBisiesto Es Verdadera. Si Es Cierto, Asigna 366 A Otra Variable Llamada Dias; De Lo Contrario, Asigna 365.
+// Comprueba Si Una Variable Nota Es Mayor O Igual A 90. Si Es Cierto, Asigna "A" A Otra Variable Llamada Calificacion; De Lo Contrario, Asigna "B".
+// Comprueba Si Una Variable Hora Es Menor A 12. Si Es Cierto, Asigna "AM" A Otra Variable Llamada Periodo; De Lo Contrario, Asigna "PM".
+// Comprueba Si Una Variable EsPositivo Es Verdadera. Si Es Cierto, Asigna El Valor Absoluto De Otra Variable Llamada Numero; De Lo Contrario, Asigna El Valor Negativo De Numero.
+// Comprueba Si Una Variable Dia Es Igual A "Lunes". Si Es Cierto, Asigna 1 A Otra Variable Llamada NumeroDia; De Lo Contrario, Asigna 0.
+// Comprueba Si Una Variable EsPrimo Es Verdadera. Si Es Cierto, Asigna "Primo" A Otra Variable Llamada Tipo; De Lo Contrario, Asigna "No Primo".
+// Comprueba Si Una Variable Edad Es Mayor O Igual A 18. Si Es Cierto, Asigna "Mayor De Edad" A Otra Variable Llamada Status; De Lo Contrario, Asigna "Menor De Edad".
+// Comprueba Si Una Variable Puntuacion Es Mayor O Igual A 60. Si Es Cierto, Asigna "Aprobado" A Otra Variable Llamada Estado; De Lo Contrario, Asigna "Reprobado".
+// Comprueba Si Una Variable EsPar Es Verdadera. Si Es Cierto, Asigna "Par" A Otra Variable Llamada TipoNumero; De Lo Contrario, Asigna "Impar".
+// Comprueba Si Una Variable Numero Es Positivo. Si Es Cierto, Asigna "Positivo" A Otra Variable Llamada Signo; De Lo Contrario, Asigna "Negativo".
+// Comprueba Si Una Variable Mes Es Igual A "Enero" O "Febrero". Si Es Cierto, Asigna "Invierno" A Otra Variable Llamada Estacion; De Lo Contrario, Asigna "Otra Estación".
+// Comprueba Si Una Variable EsDiaHabil Es Verdadera. Si Es Cierto, Asigna "Trabajo" A Otra Variable Llamada Actividad; De Lo Contrario, Asigna "Descanso".
+// Comprueba Si Una Variable EsPositivo Es Verdadera. Si Es Cierto, Asigna El Valor De Otra Variable Llamada Numero; De Lo Contrario, Asigna El Valor Negativo De Numero.
+// Comprueba Si Una Variable A Es Mayor Que Una Variable B. Si Es Cierto, Asigna El Valor De A A Otra Variable Llamada Mayor; De Lo Contrario, Asigna El Valor De B.
+// Comprueba Si Una Variable EsMayorDeEdad Es Verdadera. Si Es Cierto, Asigna "Mayor De Edad" A Otra Variable Llamada Mensaje; De Lo Contrario, Asigna "Menor De Edad".
+// Comprueba Si Una Variable Puntuacion Es Mayor O Igual A 70. Si Es Cierto, Asigna "Aprobado" A Otra Variable Llamada Resultado; De Lo Contrario, Asigna "Reprobado".
+// Comprueba Si Una Variable EsPar Es Verdadera. Si Es Cierto, Asigna "Par" A Otra Variable Llamada TipoNumero; De Lo Contrario.
+
+
+// Operadores De Tipo
+// Verifica Si Una Variable Es De Tipo Número.
+// Verifica Si Una Variable Es De Tipo Cadena De Texto.
+// Verifica Si Una Variable Es De Tipo Booleano.
+// Verifica Si Una Variable Es De Tipo Objeto.
+// Verifica Si Una Variable Es De Tipo Función.
+// Verifica Si Una Variable Es De Tipo Arreglo.
+// Verifica Si Una Variable Es De Tipo Indefinido.
+// Verifica Si Una Variable Es De Tipo Nulo.
+// Verifica Si Una Variable Es De Tipo Símbolo.
+// Verifica Si Una Variable Es De Tipo Fecha.
+// Verifica Si Una Variable Es De Tipo Regular Expression (Expresión Regular).
+// Verifica Si Una Variable Es De Tipo NaN (Not-A-Number).
+// Verifica Si Una Variable Es De Tipo Infinito.
+// Verifica Si Una Variable Es De Tipo Booleano Primitivo (True O False).
+// Verifica Si Una Variable Es De Tipo Objeto Primitivo.
+// Verifica Si Una Variable Es De Tipo Cadena De Texto Primitiva.
+// Verifica Si Una Variable Es De Tipo Número Primitivo.
+// Verifica Si Una Variable Es De Tipo Símbolo Primitivo.
+// Verifica Si Una Variable Es De Tipo Función Primitiva.
+// Verifica Si Una Variable Es De Tipo Indefinido O Nulo.
+// Verifica Si Una Variable Es De Tipo Objeto O Función.
+// Verifica Si Una Variable Es De Tipo Cadena De Texto O Número.
+// Verifica Si Una Variable Es De Tipo Objeto O Arreglo.
+// Verifica Si Una Variable Es De Tipo Cadena De Texto O Booleano.
+// Verifica Si Una Variable Es De Tipo Número O Indefinido.
+// Verifica Si Una Variable Es De Tipo Nulo O Símbolo.
