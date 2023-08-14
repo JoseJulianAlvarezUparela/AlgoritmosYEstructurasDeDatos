855a2be88dcf3dfcb8a76c32e86274ad0bc211b7
Julian <josejulianalvarezu@gmail.com>
2023-08-14 16:28:12 -0500

    Bueno Jose. Vamos a practicar mucho de los temas fetch y axios con async/await

--- js/PracticasGuiadas.js
+++ js/PracticasGuiadas.js
@@ -10,7 +10,7 @@
 
             let imagesHTML = "";
 
-            for(let i = 0; i < 5; i++){
+            for(let i = 0; i < 2; i++){
                 let image = datosImagenes[i];
                 imagesHTML += `
                 <div>
@@ -49,7 +49,7 @@
 
             let PostHTML = "";
 
-            for (let i = 0; i < 5; i++) {
+            for (let i = 0; i < 2; i++) {
                 let post = datosPost[i];
                 
                 PostHTML += `
@@ -81,3 +81,78 @@
 })();
 
 
+(() => {
+    let Comentarios = document.getElementById("comentario")
+    const xhr = new XMLHttpRequest();
+    xhr.open ("GET", "https://jsonplaceholder.typicode.com/comments")
+    xhr.responseType = 'json';
+
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300){
+            let MisComentarios = xhr.response;
+
+
+            let ComentariosHTML = "";
+
+            for(let i = 0; i < 2; i++){
+                let coment = MisComentarios[i];
+
+                ComentariosHTML += `
+                
+                <div>
+                    <h2>${coment.id}</h2>
+                    <p><${coment.email}/p>
+                    <p>${coment.name}</p>
+                    <p>${coment.body}</p>
+                </div>
+
+                `
+            }
+
+            Comentarios.innerHTML = ComentariosHTML;
+
+        } else {
+            console.llog("Error")
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error("Hubo un error")
+    }
+
+    xhr.send();
+
+})();
+
+
+// API fetch ASYNC AWAIT
+
+(() => {
+    const $fecthAsync = document.getElementById("FetchAsyncUsers"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData() {
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/users")
+
+            let json = await res.json();
+
+            if(!res.ok) throw {status: res.status, statusText: res.statusText}
+
+            json.slice(0, 5).forEach((el) => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} --- ${el.email} --- ${el.phone}`
+
+                $fragment.appendChild($li)
+            });
+
+            $fecthAsync.appendChild($fragment)
+
+        } catch (err) {
+            let messaje = err.statusText || "Error"
+            $fecthAsync.innerHTML = `Error ${err.status} - ${messaje}`
+        }
+    }
+
+    getData();
+})()
